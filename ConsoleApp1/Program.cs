3656e758980db8ea48435274af4af6ffebf8b483
Joey Anne Ulanday <joul@simcorp.com>
2024-10-07 18:05:08 +0800

    Added header in Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //JOUL
