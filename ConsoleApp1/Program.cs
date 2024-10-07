434c3bc66ed97a5c8863d3e2c2c29d53a21df4c3
Joey Anne Ulanday <joul@simcorp.com>
2024-10-07 19:59:40 +0800

    education branch

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,4 +7,5 @@ using System.Threading.Tasks;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Education - Branch");
 //JOUL
